202dd18332d82329c2974967162686d7c6713c05
Christopher Chavez <chrischavez@gmx.us>
2022-02-11 03:52:39 -0600

    proposed fix for c35c3419ea

--- generic/tkWindow.c
+++ generic/tkWindow.c
@@ -3129,6 +3129,7 @@ Initialize(
     Tcl_Obj *cmd;
 
     Tcl_Obj *nameObj = NULL;
+    Tcl_Obj *appNameObj = NULL;
     Tcl_Obj *classObj = NULL;
     Tcl_Obj *displayObj = NULL;
     Tcl_Obj *colorMapObj = NULL;
@@ -3294,6 +3295,8 @@ Initialize(
 	TkpGetAppName(interp, &nameDS);
 	nameObj = Tcl_NewStringObj(Tcl_DStringValue(&nameDS),
 		Tcl_DStringLength(&nameDS));
+	appNameObj = nameObj;
+	Tcl_IncrRefCount(appNameObj);
 	Tcl_DStringFree(&nameDS);
     }
 
@@ -3454,6 +3457,10 @@ tkInit", -1, TCL_EVAL_GLOBAL);
     if (value) {
 	Tcl_DecrRefCount(value);
 	value = NULL;
+    }
+    if (appNameObj) {
+	Tcl_DecrRefCount(appNameObj);
+	appNameObj = NULL;
     }
     return code;
 }
