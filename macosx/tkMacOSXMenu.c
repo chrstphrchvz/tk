46b0fb1d1de87ad974d61c12c238d274a937208b
Christopher Chavez <chrischavez@gmx.us>
2022-08-07 13:49:26 -0500

    proposed fix for ca2ebe1fca

--- macosx/tkMacOSXMenu.c
+++ macosx/tkMacOSXMenu.c
@@ -930,8 +930,6 @@ TkpPostMenu(
 	}
 	realWin = Tk_Parent(realWin);
     }
-    NSWindow *win = [realWinView window];
-    NSView *view = [win contentView];
     NSMenu *menu = (NSMenu *) menuPtr->platformData;
     NSInteger itemIndex = index;
     NSInteger numItems = [menu numberOfItems];
@@ -961,8 +959,9 @@ TkpPostMenu(
     }
 
     [menu popUpMenuPositioningItem:item
-			atLocation:[win tkConvertPointFromScreen:location]
-			    inView:view];
+			atLocation:location
+			    inView:nil
+			appearance:realWinView.effectiveAppearance];
     inPostMenu = 0;
     return TCL_OK;
 }
